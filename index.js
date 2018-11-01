1bcd5d2e3da3373a05cc88c6dab8acfda0695f86
Ihor Verkhohliad <ihor.verkhohliad@gmail.com>
2018-11-01 16:44:37 +0200

    Revert "Dev"

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-getColors = require('./scripts/getColors');
-createColumn = require('./scripts/createColumn');
-goDance = require('./scripts/partyMethods').goDance;
-addAnimClass = require('./scripts/partyMethods').addAnimClass;
-partyStart = require('./scripts/partyMethods').partyStart;
-checkColor = require('./scripts/paletteMethods').checkColor;
-palette = require('./scripts/paletteMethods').palette;
-shaderColor = require('./scripts/paletteMethods').shaderColor;
-Brick = require('./scripts/Brick');
-PopUpMessage = require('./scripts/PopUpMessage');
 
+alert('im index.js');
 
-// Контейнер для PopUp сообщений
-const popUpContainer = document.createElement('div');
-popUpContainer.className = 'popUpContainer';
-document.body.appendChild(popUpContainer);
+requireJS('./scripts/init.js')
+    .then(() => {
+        alert("init is ok!");
+        newDiv();
 
-//Контейнер для таблицы градиента
-const container = document.createElement('div');
-container.className = 'container';
-document.body.appendChild(container);
-
-//Получили массив цветов, создаём и монтируем блоки
-getColors()
-.then(res => {
-    let fragment = document.createDocumentFragment();
-    res.forEach((item) => {
-    const column = createColumn("#" + item);
-    fragment.appendChild(column);
-  })
-  container.appendChild(fragment);
-  partyStart();
-})
+    })
+    .catch((err) => {
+        console.log(err);
+    });
--- scripts/PopUpMessage.js
+++
@@ -1,8 +0,0 @@
-// Уведомление о копировании
- module.exports = class {
-    constructor(msg) {
-      this.element = document.createElement('div');
-      this.element.className = 'message';
-      this.element.innerHTML = 'Color '+ msg + ' is copied to clipboard!';
-    }
-}
